cec218d263c3e735ddeda0effb241e8b6f22ff89
Aleksey Morarash <aleksey.morarash@gmail.com>
2014-12-12 11:22:47 +0200

    slightly refactored...
    - do not extract messages from the topic beginning by default;
    - add -b command line option to extract messages from the
     topic beginning;
    - commit current position before termination;
    - add code comments.

--- kafcat.py
+++ kafcat.py
@@ -20,34 +20,43 @@ def usage(exitcode = 1):
         ' Options:\n'
         '  --host Host   Kafka node hostname. Default is "localhost";\n'
         '  --port Port   Kafka node port number. Default is 9092;\n'
+        '  -b            extract from the beginning;\n'
         '  -f            output appended data as the topic grows.\n')
     sys.exit(exitcode)
 
 
 if __name__ == '__main__':
+    # Parse command line options
     try:
         cmd_opts, cmd_args = getopt.getopt(
-            sys.argv[1:], 'f', ['host=', 'port='])
+            sys.argv[1:], 'bf', ['host=', 'port='])
         cmd_opts = dict(cmd_opts)
     except getopt.GetoptError as exc:
         sys.stderr.write('Error: ' + str(exc) + '\n')
         usage()
     if len(cmd_args) != 1:
         usage()
-    host = cmd_opts.get('--host', 'localhost')
-    port = int(cmd_opts.get('--port', '9092'))
-    tailf = '-f' in cmd_opts
-    topic = cmd_args[0]
+    consumer = None
     try:
+        # connect to Kafka
+        host = cmd_opts.get('--host', 'localhost')
+        port = int(cmd_opts.get('--port', '9092'))
         client = kafka.KafkaClient(host + ':' + str(port))
-        if tailf:
-            consumer = kafka.SimpleConsumer(
-                client, 'kafcat', topic)
-        else:
-            consumer = kafka.SimpleConsumer(
-                client, 'kafcat', topic, iter_timeout = 0.5)
-        consumer.seek(0, 0)
+        # Create consumer object
+        consumer = kafka.SimpleConsumer(
+            client, 'kafcat', cmd_args[0],
+            iter_timeout = None if '-f' in cmd_opts else 0.5)
+        if '-b' in cmd_opts:
+            # seek very first message
+            consumer.seek(0, 0)
+            consumer.commit()
+        # start consuming
         for message in consumer:
-            print(message.message.value)
+            sys.stdout.write(message.message.value + '\n')
+        # save current position for the future use
+        consumer.commit()
     except KeyboardInterrupt:
+        if consumer is not None:
+            # save current position for the future use
+            consumer.commit()
         sys.exit(130)
