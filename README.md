513af6d451252ecbff11be50a4463f118a403751
Aleksey Morarash <aleksey.morarash@gmail.com>
2015-04-21 15:16:56 +0300

    add --id cmd option

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ Options:
 
 * _--host Host_ - Kafka node hostname. Default is "localhost";
 * _--port Port_ - Kafka node port number. Default is 9092;
+* _--id ClientID_ - Kafka client ID. Default is 'kafcat';
 * _--maxtime Seconds_ - max time to work. Default is no limit;
 * _-f_ - output appended data as the topic grows.
 
--- kafcat.py
+++ kafcat.py
@@ -21,6 +21,8 @@ def usage(exitcode = 1):
         ' Options:\n'
         '  --host Host   Kafka node hostname. Default is "localhost";\n'
         '  --port Port   Kafka node port number. Default is 9092;\n'
+        '  --id ClientID\n'
+        '                Kafka client ID. Default is \'kafcat\';\n'
         '  --maxtime Seconds\n'
         '                Maximum time to work. If defined, kafcat will\n'
         '                exit after the given amount of time even if there\n'
@@ -34,7 +36,7 @@ if __name__ == '__main__':
     # Parse command line options
     try:
         cmd_opts, cmd_args = getopt.getopt(
-            sys.argv[1:], 'bf', ['host=', 'port=', 'maxtime='])
+            sys.argv[1:], 'bf', ['host=', 'port=', 'id=', 'maxtime='])
         cmd_opts = dict(cmd_opts)
     except getopt.GetoptError as exc:
         sys.stderr.write('Error: ' + str(exc) + '\n')
@@ -46,13 +48,14 @@ if __name__ == '__main__':
         # connect to Kafka
         host = cmd_opts.get('--host', 'localhost')
         port = int(cmd_opts.get('--port', '9092'))
+        client_id = cmd_opts.get('--id', 'kafcat')
         maxtime = cmd_opts.get('--maxtime', None)
         if maxtime is not None:
             maxtime = int(maxtime)
         client = kafka.KafkaClient(host + ':' + str(port))
         # Create consumer object
         consumer = kafka.SimpleConsumer(
-            client, 'kafcat', cmd_args[0], iter_timeout = 0.5)
+            client, client_id, cmd_args[0], iter_timeout = 0.5)
         if '-b' in cmd_opts:
             # seek very first message
             consumer.seek(0, 0)
