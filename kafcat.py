e9e921461fd6fa5a361b3a061d54d2203620eb21
Aleksey Morarash <aleksey.morarash@gmail.com>
2015-04-21 15:16:26 +0300

    add --maxtime cmd option

--- kafcat.py
+++ kafcat.py
@@ -8,6 +8,7 @@ import getopt
 import kafka
 import os.path
 import sys
+import time
 
 
 def usage(exitcode = 1):
@@ -20,6 +21,10 @@ def usage(exitcode = 1):
         ' Options:\n'
         '  --host Host   Kafka node hostname. Default is "localhost";\n'
         '  --port Port   Kafka node port number. Default is 9092;\n'
+        '  --maxtime Seconds\n'
+        '                Maximum time to work. If defined, kafcat will\n'
+        '                exit after the given amount of time even if there\n'
+        '                is data to read in the Kafka;\n'
         '  -b            extract from the beginning;\n'
         '  -f            output appended data as the topic grows.\n')
     sys.exit(exitcode)
@@ -29,7 +34,7 @@ if __name__ == '__main__':
     # Parse command line options
     try:
         cmd_opts, cmd_args = getopt.getopt(
-            sys.argv[1:], 'bf', ['host=', 'port='])
+            sys.argv[1:], 'bf', ['host=', 'port=', 'maxtime='])
         cmd_opts = dict(cmd_opts)
     except getopt.GetoptError as exc:
         sys.stderr.write('Error: ' + str(exc) + '\n')
@@ -41,18 +46,28 @@ if __name__ == '__main__':
         # connect to Kafka
         host = cmd_opts.get('--host', 'localhost')
         port = int(cmd_opts.get('--port', '9092'))
+        maxtime = cmd_opts.get('--maxtime', None)
+        if maxtime is not None:
+            maxtime = int(maxtime)
         client = kafka.KafkaClient(host + ':' + str(port))
         # Create consumer object
         consumer = kafka.SimpleConsumer(
-            client, 'kafcat', cmd_args[0],
-            iter_timeout = None if '-f' in cmd_opts else 0.5)
+            client, 'kafcat', cmd_args[0], iter_timeout = 0.5)
         if '-b' in cmd_opts:
             # seek very first message
             consumer.seek(0, 0)
             consumer.commit()
         # start consuming
-        for message in consumer:
-            sys.stdout.write(message.message.value + '\n')
+        started = time.time()
+        while True:
+            if maxtime is not None and time.time() >= started + maxtime:
+                break
+            for message in consumer:
+                sys.stdout.write(message.message.value + '\n')
+                if maxtime is not None and time.time() >= started + maxtime:
+                    break
+            if '-f' not in cmd_opts:
+                break
         # save current position for the future use
         consumer.commit()
     except KeyboardInterrupt:
